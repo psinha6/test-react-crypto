9ee7d52747b73dca00aa8a29f0b2ae4a2373a4cf
Prateek Sinha <psinha6@gmail.com>
2022-12-23 10:12:33 +0100

    Added footer css

--- src/components/details-footer/details-footer.styles.jsx
+++ src/components/details-footer/details-footer.styles.jsx
@@ -1,15 +1,18 @@
 import styled from "styled-components";
 
 export const Banner = styled.div`
-    width: 100%;
+    width: 98%;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
     font-weight: bolder;
+    background-color: #111111;
+    padding: 10px;
     p {
         font-weight: bolder;
         font-size: 30px;
+        color: #fff;
     }
 `
 
@@ -18,4 +21,8 @@ export const WebSites = styled.div`
     flex-wrap: wrap;
     width: 100%;
     justify-content: space-around;
+    a {
+        color: white;
+        text-decoration: none;
+    }
 `
